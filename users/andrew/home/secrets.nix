d3c8c8a9bf55c4d73004bf8046b3bc9a39237365
andrew <69527486+thevndrew@users.noreply.github.com>
2024-05-03 15:20:43 -0400

    Add more secrets

--- users/andrew/home/secrets.nix
+++ users/andrew/home/secrets.nix
@@ -2,10 +2,21 @@
 {
   sops = {
     age.sshKeyPaths = [ "${sopsKey}" ];
-    defaultSopsFile = "${inputs.mysecrets}/secrets/services_env.yaml";
-    secrets.services_env = {
-      sopsFile = "${inputs.mysecrets}/secrets/services_env.yaml";
-      path = "${systemInfo.home}/.config/services.env"; 
+    defaultSopsFile = "${inputs.mysecrets}/secrets/services.yaml";
+
+    secrets."services/env" = {
+      sopsFile = "${inputs.mysecrets}/secrets/services.yaml";
+      path = "${systemInfo.home}/.config/services/services.env"; 
+    };
+
+    secrets."services/${systemInfo.hostname}" = {
+      sopsFile = "${inputs.mysecrets}/secrets/services.yaml";
+      path = "${systemInfo.home}/.config/services/${systemInfo.hostname}.yaml"; 
+    };
+
+    secrets."ytdl" = {
+      sopsFile = "${inputs.mysecrets}/secrets/ytdl.yaml";
+      path = "${systemInfo.home}/.config/ytdl/streams.yaml"; 
     };
   };
 }
