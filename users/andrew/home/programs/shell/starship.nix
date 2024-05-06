28a61bdb3cef90b7995c8b3a54ec036469f4b7fe
andrew <69527486+thevndrew@users.noreply.github.com>
2024-05-06 08:51:54 -0400

    Remove tmux alias

--- users/andrew/home/programs/shell/starship.nix
+++ users/andrew/home/programs/shell/starship.nix
@@ -6,5 +6,21 @@ in
   programs.starship = import ../integration_settings.nix // {
     enable = true;
     package = unstable.starship;
+    settings = {
+      palette = "gruvbox_dark";
+
+      palettes.gruvbox_dark = {
+        color_fg0 = "#fbf1c7";
+        color_bg1 = "#3c3836";
+        color_bg3 = "#665c54";
+        color_blue = "#458588";
+        color_aqua = "#689d6a";
+        color_green = "#98971a";
+        color_orange = "#d65d0e";
+        color_purple = "#b16286";
+        color_red = "#cc241d";
+        color_yellow = "#d79921";
+      };
+    };
   };
 }
--- users/andrew/home/programs/zellij.nix
+++ users/andrew/home/programs/zellij.nix
@@ -3,13 +3,17 @@ let
   unstable = other-pkgs.unstable;
   shellAliases = {
     "zj" = "zellij";
-    "tmux" = "zellij";
   };
 in
 {
   programs.zellij = import ./integration_settings.nix // {
     enable = true;
     package = unstable.zellij;
+    settings = {
+      theme = "gruvbox-dark";
+      #theme = "custom"
+      #themes.custom.fg = "#ffffff";
+    };
   };
 
   home.shellAliases = shellAliases;
