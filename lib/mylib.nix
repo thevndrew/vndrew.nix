0082de156b17cbe0a388f652efed043d24bc60ad
andrew <69527486+thevndrew@users.noreply.github.com>
2024-05-11 16:41:00 -0400

    Update mylib filtering

--- lib/mylib.nix
+++ lib/mylib.nix
@@ -4,17 +4,31 @@
   relativeToRoot = lib.path.append ../.;
 
   scanPaths = path:
+  let
+    excludeDirs = [
+      "scripts"
+    ];
+
+    excludeNameSuffix = [
+      "settings"
+    ];
+
+    filterDir = path: builtins.any (dir: path != dir) excludeDirs;
+    filterFileSuffix = path: builtins.any (suffix: !lib.strings.hasSuffix "${suffix}.nix" path) excludeNameSuffix;
+  in
     builtins.map
     (f: (path + "/${f}"))
     (builtins.attrNames
       (lib.attrsets.filterAttrs
         (
-          path: _type:
-            (_type == "directory" && (path != "scripts")) # include directories
+          path: _type: (
+              _type == "directory"       # include directories
+              && filterDir path          # filter excluded dirs
+            )
             || (
-              (path != "default.nix") # ignore default.nix
-              && (!lib.strings.hasSuffix "settings.nix" path) # ignore *settings.nix
-              && (lib.strings.hasSuffix ".nix" path) # include .nix files
+              (path != "default.nix")    # ignore default.nix
+              && (filterFileSuffix path) # ignore *settings.nix files
+              && (lib.strings.hasSuffix ".nix" path)  # include .nix files
             )
         )
         (builtins.readDir path)));
