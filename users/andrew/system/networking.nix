2066dd56c70359a0219e6c87893834f52e4e9b7f
andrew <69527486+thevndrew@users.noreply.github.com>
2024-08-03 11:22:57 -0400

    Update network settings

--- users/andrew/system/networking.nix
+++ users/andrew/system/networking.nix
@@ -21,25 +21,21 @@ in {
         # enable the firewall
         enable = true;
 
-        interfaces."podman+".allowedUDPPorts = [53];
-
         # always allow traffic from your Tailscale network
         trustedInterfaces = ["tailscale0"];
 
-        # allow the Tailscale UDP port through the firewall
-        allowedUDPPorts = [config.services.tailscale.port];
-
         # allow you to SSH in over the public internet
-        allowedTCPPorts = [22 2222];
+        allowedTCPPorts = [
+          22
+          2222
+        ];
       };
       nameservers = [
         "100.100.100.100"
         "9.9.9.9"
-        "10.89.0.1"
       ];
       search = [
         "ainu-kanyu.ts.net"
-        "dns.podman"
       ];
     };
   };
