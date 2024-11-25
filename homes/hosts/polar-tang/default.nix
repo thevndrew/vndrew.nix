1d4753ae0aaf27a1866da61e60678d1958ff35f6
andrew <69527486+thevndrew@users.noreply.github.com>
2024-11-24 22:58:18 -0500

    Try to fix WSL history

--- homes/hosts/polar-tang/default.nix
+++ homes/hosts/polar-tang/default.nix
@@ -7,4 +7,8 @@
     pbpaste = "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -command 'Get-Clipboard'";
     explorer = "/mnt/c/Windows/explorer.exe";
   };
+
+  sops = {
+    age.sshKeyPaths = ["/mnt/c/Users/AJ-XPS/.ssh"];
+  };
 }
