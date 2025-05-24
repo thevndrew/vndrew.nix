6a00490fa0910051af75ddd8639b2a8cc6acd2e9
andrew <69527486+thevndrew@users.noreply.github.com>
2025-05-24 16:23:26 -0400

    update flake (switch to stable)

--- flake.nix
+++ flake.nix
@@ -19,16 +19,13 @@
   # };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    # nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
-    nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-24.11";
-    # nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nur.url = "github:nix-community/NUR";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
-      # url = "github:nix-community/home-manager?ref=release-25.05";
+      url = "github:nix-community/home-manager?ref=release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
